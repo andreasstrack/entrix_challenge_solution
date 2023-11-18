2fae0486a62d27e632afd0321d452409e8e3b17a
Andreas Strack <andreas.strack@web.de>
2023-11-18 12:11:08 +0100

    Add test to assert testing in pipeline

--- test/code_pipeline.test.ts
+++ test/code_pipeline.test.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import {Template} from 'aws-cdk-lib/assertions';
 import {CodePipelineStack} from "../lib/code_pipeline/code_pipeline-stack";
+import * as assert from "assert";
 
 test('Pipeline Has Expected Steps', () => {
     const app = new cdk.App();
@@ -21,4 +22,21 @@ test('Pipeline Has Expected Steps', () => {
             {"Name": "Prod"},
         ]
     });
+});
+
+test('Tests Run in Pipeline', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new CodePipelineStack(
+        app, 'MyTestStack', {env: {account: '704868603297', region: 'eu-west-2'}});
+    // THEN
+    const template = Template.fromStack(stack);
+
+    const buildProjects = template.findResources("AWS::CodeBuild::Project", {})
+    const cdkBuildKey = Object.keys(buildProjects)
+        .filter(key => key.startsWith("EntrixChallengePipelineBuildSynthCdkBuildProject"))[0];
+    const cdkBuildProject = buildProjects[cdkBuildKey];
+    const buildCommands: string[] = cdkBuildProject["Properties"]["Source"]["BuildSpec"].split("\n")
+    const npmTestCommandString = buildCommands.filter( command => command.includes('"npm test"'))[0]
+    assert(npmTestCommandString != undefined)
 });
